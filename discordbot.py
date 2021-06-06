dd385cf07ab6e80f9897e1e697ba2a1e9b84488d
pauracongereel <85428609+pauracongereel@users.noreply.github.com>
2021-06-06 16:11:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,15 +17,10 @@ async def on_command_error(ctx, error):
 async def oguricap(ctx):
     await ctx.send('https://cdn.discordapp.com/attachments/850978385412882445/850987498242572308/05d2870175baf606.jpg')
     await ctx.send('スピードA846　スタミナB691　パワーB+799　根性E+292　賢さC498')
-@bot.command()
-async def オグリキャップ(ctx):
-    await ctx.send('https://cdn.discordapp.com/attachments/850978385412882445/850987498242572308/05d2870175baf606.jpg')
 
 @bot.command()
 async def silencesuzuka(ctx):
-    await ctx.send('https://cdn.discordapp.com/attachments/850978385412882445/850987501422116874/9e8277cce9ad01c5.jpg')
-@bot.command()
-async def サイレンススズカ(ctx):
-    await ctx.send('https://cdn.discordapp.com/attachments/850978385412882445/850987501422116874/9e8277cce9ad01c5.jpg')    
+    await ctx.send('https://cdn.discordapp.com/attachments/850978385412882445/850987501422116874/9e8277cce9ad01c5.jpg')  
+    await ctx.send('スピードA+985　スタミナC+509　パワーC+515　根性D329　賢さC414')
     
 bot.run(token)
